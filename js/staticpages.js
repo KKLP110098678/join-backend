8c65930425d6c9953f6e2548f20454d115e2a256
Wassim Madwar <wassimmadwar@gmail.com>
2025-12-15 08:05:53 +0100

    Remove unused CSS and JavaScript code for improved maintainability

--- js/staticpages.js
+++ js/staticpages.js
@@ -1,33 +1,53 @@
 function initStaticPages() {
-  let isLoggedIn = sessionStorage.getItem("isLoggedIn");
-  if (isLoggedIn === "true") {
-    return;
-  } else {
-    renderLoginSidbar();
-    renderEmptyHeader();
-  }
+	const isLoggedIn = sessionStorage.getItem("isLoggedIn");
+	if (isLoggedIn === "true") {
+		return;
+	} else {
+		renderLoginSidbar();
+		renderEmptyHeader();
+	}
 }
 
-
 function renderLoginSidbar() {
-  let = emptySidebar = document.getElementById("sidebar-nav");
-  emptySidebar.innerHTML = "";
-  emptySidebar.innerHTML = `
-      <nav class="sidenav d-flex">
-        <a class="btn-menu" href="./login.html"
+	const emptySidebar = document.getElementById("sidebar-nav");
+  emptySidebar.classList.remove("sidenav");
+	emptySidebar.innerHTML = "";
+	emptySidebar.innerHTML = `
+      <div class="empty-nav">
+
+<div>
+<a class="btn-menu" href="./login.html"
           ><img
             src="/assets/icon/sign/login.svg"
             alt="Add Task Icon"
           />Log In</a
         >
-      </nav>
+</div>
+
+ <div class="sidebar-links">
+	<a href="./datenschutz.html" class="selected-link">
+		Privacy Policy
+	</a>
+	<a href="./legalnotice.html" class="pad-8">
+		Legal Notice
+	</a>
+</div>
+        </div>
+
   `;
 }
 
-
 function renderEmptyHeader() {
-  let userHeadere = document.getElementById("menu-user");
-  userHeadere.innerHTML = "";
-  userHeadere.innerHTML = `<p>Kanban Project Management Tool</p>`;
+	const userHeadere = document.getElementById("menu-user");
+	userHeadere.innerHTML = "";
+	userHeadere.innerHTML = `<p>Kanban Project Management Tool</p>`;
 }
- 
+
+// {/* <div class="sidebar-footer d-flex">
+// 	<a href="./datenschutz.html" class="selected-link">
+// 		Privacy Policy
+// 	</a>
+// 	<a href="./legalnotice.html" class="pad-8">
+// 		Legal Notice
+// 	</a>
+// </div>; */}
